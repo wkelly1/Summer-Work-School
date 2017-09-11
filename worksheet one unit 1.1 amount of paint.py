61ed10826ba24506ee9c49f8c274779e3561c3ae
wkelly1 <william.kelly20@btinternet.com>
2017-09-11 20:52:15 +0100

    Unit 1.1 work sheet
    These are in python

---
+++ worksheet one unit 1.1 amount of paint.py
@@ -0,0 +1,37 @@
+command = True
+while command:
+    try:
+        size = input("How large are your walls in square meters? ")
+        size = float(size)
+        command = False
+    except ValueError:
+        print("This is not a number")
+        
+command = True
+while command:
+    try:
+        unpaintable = input("How large are the areas that you dont want painting in square meters? ")
+        unpaintable = float(unpaintable)
+        command = False
+    except ValueError:
+        print("This is not a number")
+
+command = True
+while command:
+    try:
+        nolayers = int(input("How many layers do you want? "))
+        nolayers = float(nolayers)
+        command = False
+        
+    except ValueError:
+        print("This is not a number")
+
+def amountofpaint(a,b,c):
+    amountofpaint = ((a - b) * c) / 11
+    return(amountofpaint)
+
+
+
+
+
+print("You need", round(amountofpaint(size, unpaintable, nolayers), 3), "litres of paint")
---
+++ worksheet one unit 1.1.py
@@ -0,0 +1,10 @@
+import pdb
+a = 5
+b = 10
+pdb.set_trace()
+c = 6
+answer1 = a + b * c
+print ("answer1 =", answer1)
+answer2 = (a + b) * c
+print ("answer2 =", answer2)
+
